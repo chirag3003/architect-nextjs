60ee397a08af4f21f8b6e10fa8b78988df2a30d2
= <=>
2023-12-02 17:29:47 +0530

    updated images

--- configs/carousels.ts
+++ configs/carousels.ts
@@ -1,9 +1,14 @@
 export const imageCarousel = [
-    {
-        src: "/images/image1.jpg",
-        text: "Image 1",
-    }, {
-        src: "/images/image2.jpg",
-        text: "Image 2"
-    }
-]
+  {
+    src: "/images/image1.jpg",
+    text: "Image 1",
+  },
+  {
+    src: "/images/image2.jpg",
+    text: "Image 2",
+  },
+  {
+    src: "/images/image3.jpg",
+    text: "Image 2",
+  },
+];
