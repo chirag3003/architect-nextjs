7000025835cc1214feb94e78ed20f076150ac9aa
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-03-28 22:26:00 +0530

    logo

--- configs/clients.ts
+++ configs/clients.ts
@@ -1,25 +1,33 @@
 const clients = [
-    {
-        title: "Sony",
-        img: "./sony.png",
-        text: "Associated since 1995"
-    },
-    {
-        title: "Ceat",
-        img: "./ceat.png",
-    },
-    {
-        title: "CreamCentre",
-        img: "./creamcenter.png",
-    },
-    {
-        title: "Sanofi",
-        img: "./sanofi.svg",
-    },
-    {
-        title: "VIP",
-        img: "./vip.png"
-    }
+  {
+    title: "Sony",
+    img: "./sony.png",
+    text: "Associated since 1995",
+  },
+  {
+    title: "Ceat",
+    img: "./ceat.png",
+  },
+  {
+    title: "CreamCentre",
+    img: "./creamcenter.png",
+  },
+  {
+    title: "Sanofi",
+    img: "./sanofi.svg",
+  },
+  {
+    title: "VIP",
+    img: "./vip.png",
+  },
+  {
+    title: "",
+    img: "./sahara-india.png",
+  },
+  {
+    title: "",
+    img: "./hindustan.png",
+  },
 ];
 
 export default clients;
